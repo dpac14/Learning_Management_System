25b700ee66f1ef2ebf995a853514616cee63eb08
dpac14 <dipakchute11@gmail.com>
2026-01-26 23:27:04 +0530

    Changes in Add couser file

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const AddCourse = () => {
-  return (
-    <div>
-      <h1>Add Course</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Add Course</h1>
+        </div>
+    );
 }
 
 export default AddCourse;
