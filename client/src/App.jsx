ea676ec7e21cbfb9f11cbaec58856b089c40e9b7
dpac14 <dipakchute11@gmail.com>
2026-02-02 13:08:39 +0530

    Added Some New Files and  Attached Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/student/Home';
+import CourseList from './pages/student/CourseList';
+import CourseDetails from './pages/student/CourseDetails';
+import MyEnrollments from './pages/student/MyEnrollments';
+import Player from './pages/student/Player';
+import Loading from './components/students/Loading';
+import Educator from './pages/educator/Educator';
+import DashBord from './pages/educator/DashBord';
+import AddCourse from './pages/educator/AddCourse';
+import MyCourse from './pages/educator/MyCourse';
+import StudentEnrolled from './pages/educator/StudentEnrolled';
 const App = () => {
   return (
+
     <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/course-list' element={<CourseList />} />
+        <Route path='/course-list/:input' element={<CourseList />} />
+        <Route path='/course/:id' element={<CourseDetails />} />
+        <Route path='/my-enrollments' element={<MyEnrollments />} />
+        <Route path='/player/:coursedId' element={<Player />} />
+        <Route path='/loading/:path' element={<Loading />} />
+        <Route path='/educator' element={<Educator />}>
+              <Route path='educator' element={<DashBord />} />
+              <Route path='add-course' element={<AddCourse />} />
+              <Route path='my-courses' element={<MyCourse />} />
+              <Route path='student-enrolled' element={< StudentEnrolled />} />
       
+        </Route>
+
+      </Routes>
     </div>
   );
 }
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,12 +3,10 @@ import { createContext } from "react";
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-    const value = {
-
-    }
+    const value = {};
     return (
         <AppContext.Provider value={value}>
-            {props.childern}
+            {props.children}
         </AppContext.Provider>
     )
 }
