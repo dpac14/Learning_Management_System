d92e2409e66714e37702261391cb067f6b1c356c
dpac14 <dipakchute11@gmail.com>
2026-02-22 19:41:09 +0530

    Added Course Card

--- client/src/components/students/CoursesSection.jsx
+++ client/src/components/students/CoursesSection.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import CourseCard from './CourseCard';
 
 const CoursesSection = () => {
-  return (
-    <div className='py-16 md:px-40 px-8'>  
-      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
-      <p className='text-sm md:text-base text-gray-500 mt-3'> Discover our to-p courses across vai=rious categories, From coding and design to business and wellness, our courses are crated to deliver result.</p>
 
-      <Link to={'/course-list'} onClick={()=> scrollTo(0,0)}
-      className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
+  const { allCourses } = useContext(AppContext)
+  return (
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'> Discover our to-p courses across vai=rious categories, From coding and design to <br />business and wellness, our courses are crated to deliver result.</p>
+      <div className='grid grid-auto-fit gap-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
+        {allCourses.slice(0, 4).map((course, index) => <CourseCard key={index} course={course} />)}
+      </div>
+      <Link to={'/course-list'} onClick={() => scrollTo(0, 0)}
+        className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
       >Show all courses</Link>
     </div>
   );
