5adb93864eb096357035d9a13dc8e4c23cfe47af
dpac14 <dipakchute11@gmail.com>
2026-02-22 19:38:13 +0530

    Added some values

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,29 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-    const value = {};
+
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const navigate = useNavigate()
+
+    const [allCourses, setAllCourses] = useState([])
+
+    // Fetch all Courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(() => {
+        fetchAllCourses()
+    }, [])
+
+    const value = {
+        currency, allCourses, navigate
+    };
     return (
         <AppContext.Provider value={value}>
             {props.children}
