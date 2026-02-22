707ba58553356ffe8d3775680d0236555d551596
dpac14 <dipakchute11@gmail.com>
2026-02-22 15:04:28 +0530

    Added Courses Cad Section

--- client/src/components/students/CoursesSection.jsx
+++ client/src/components/students/CoursesSection.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CoursesSection = () => {
   return (
-    <div>
-      <h1>Courses section</h1>
+    <div className='py-16 md:px-40 px-8'>  
+      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'> Discover our to-p courses across vai=rious categories, From coding and design to business and wellness, our courses are crated to deliver result.</p>
+
+      <Link to={'/course-list'} onClick={()=> scrollTo(0,0)}
+      className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
+      >Show all courses</Link>
     </div>
   );
 }
