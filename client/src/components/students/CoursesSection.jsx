990f9bde3bdef400a3c55f5b5134ac8990c19048
dpac14 <dipakchute11@gmail.com>
2026-02-23 13:22:05 +0530

    Changese some line

--- client/src/components/students/CoursesSection.jsx
+++ client/src/components/students/CoursesSection.jsx
@@ -10,7 +10,7 @@ const CoursesSection = () => {
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
       <p className='text-sm md:text-base text-gray-500 mt-3'> Discover our to-p courses across vai=rious categories, From coding and design to <br />business and wellness, our courses are crated to deliver result.</p>
-      <div className='grid grid-auto-fit gap-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
+      <div className='grid grid-auto-fit gap-4 px-4 md:px-0 md:my-16 my-10'>
         {allCourses.slice(0, 4).map((course, index) => <CourseCard key={index} course={course} />)}
       </div>
       <Link to={'/course-list'} onClick={() => scrollTo(0, 0)}
