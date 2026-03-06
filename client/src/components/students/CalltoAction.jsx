2c28998ed8494fdf0bb33a78703f06d1052b8ad1
dpac14 <dipakchute11@gmail.com>
2026-03-06 12:55:12 +0530

    Add Some Changes

--- client/src/components/students/CalltoAction.jsx
+++ client/src/components/students/CalltoAction.jsx
@@ -8,7 +8,7 @@ const CalltoAction = () => {
       <p className='text-gray-500 sm:text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit quia quo maxime temporibus, quisquam enim saepe consectetur accusamus quibusdam praesentium</p>
       <div className='flex items-center font-medium gap-6 mt-4'>
         <button className='px-10 py-3 rounded-md text-white bg-blue-600'>Get Stared</button>
-        <button className='flex items-center gap-2'>Lern more <img src={assets.arrow_icon}alt="arrow_icon" /></button>
+        <button className='flex items-center gap-2'>Lern more <img src={assets.arrow_icon} alt="arrow_icon" /></button>
       </div>
     </div>
   );
