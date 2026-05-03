81e75971ccf6dad49d8d1f2271f4395f6303f93f
dpac14 <dipakchute11@gmail.com>
2026-05-03 20:29:17 +0530

    added new line

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -8,10 +8,21 @@ const CourseList = () => {
 
   const { navigate, allCourses } = useContext(AppContext)
   const { input } = useParams()
-  const [filteredCourse, setFilteredCourse] = useState([])
-  useEffect(() => ({
+  const [filteredCourse, setFilteredCourse] =useState([])
+  useEffect(()=>{
+    if(allCourses && allCourses.length > 0){
+      const tempCourses = allCourses.slice()
 
-  }, [allCourses, input]))
+      input ?
+      setFilteredCourse(
+        tempCourses.filter(
+          item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+        )
+      )
+      : setFilteredCourse(tempCourses)
+    }
+
+  },[allCourses, input])
 
   return (
     <>
@@ -25,7 +36,7 @@ const CourseList = () => {
           <SearchBar data={input} />
         </div>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0' >
-          {allCourses.map((course, index) => <CourseCard key={index} course={course} />)}
+          {filteredCourse.map((course, index) => <CourseCard key={index} course={course} />)}
         </div>
       </div>
     </>
