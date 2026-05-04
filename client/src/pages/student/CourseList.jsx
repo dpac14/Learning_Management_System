3d815b94c1b55e6485a538d541a441579d931e66
dpac14 <dipakchute11@gmail.com>
2026-05-04 12:24:56 +0530

    Complet The Course list page

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -3,6 +3,8 @@ import { AppContext } from '../../context/AppContext';
 import SearchBar from '../../components/students/SearchBar';
 import { useParams } from 'react-router-dom';
 import CourseCard from '../../components/students/CourseCard';
+import { assets } from '../../assets/assets';
+import Footer from '../../components/students/Footer';
 
 const CourseList = () => {
 
@@ -35,10 +37,18 @@ const CourseList = () => {
           </div>
           <SearchBar data={input} />
         </div>
+
+        {
+          input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600'>
+            <p>{input}</p>
+            <img src={assets.cross_icon} alt=""className='cursor-pointer' onClick={()=>navigate('/course-list')} />
+          </div>
+        }
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0' >
           {filteredCourse.map((course, index) => <CourseCard key={index} course={course} />)}
         </div>
       </div>
+     <Footer />
     </>
   );
 }
