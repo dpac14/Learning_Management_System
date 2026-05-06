44ac8bc6e85b485fd2ae6bc55d26b71e459a9801
dpac14 <dipakchute11@gmail.com>
2026-05-06 17:49:23 +0530

    Added Course detail left side

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,13 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
+import Loading from '../../components/students/Loading';
+import { assets } from '../../assets/assets';
 
 const CourseDetails = () => {
 
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null)
 
-  const { allCourses } = useContext(AppContext)
+  const { allCourses,calculateRating } = useContext(AppContext)
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id)
@@ -17,12 +19,31 @@ const CourseDetails = () => {
     fetchCourseData()
   }, [])
 
-  return (
+  return courseData ? (
+    <>
     <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
-      <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+      <div className='absolute top-0 left-0 w-full h-spacing-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
 
       {/* let colunm */}
-      <div></div>
+      <div className='max-w-xl z-10 text-gray-500'>
+        <h1 className='md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800'>{courseData.courseTitle}</h1>
+        <p className='pt-4 md:text-base text-sm '
+         dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0,200)}}></p>
+
+{/* review and rating  */}
+<div className='flex items-center space-x-2'>
+          <p>{calculateRating(courseData)}</p>
+          <div className='flex'>
+            {[...Array(5)].map((_, i) => (<img key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt='' className='w-3.5 h-33.5' />
+            ))}
+          </div> 
+           <p className='text-blue-600'>({courseData.courseRatings.length} {courseData.courseRatings.length > 1 ?'ratings ': 'rating'})</p>
+
+           <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' : 'student' }</p>
+        </div>
+
+
+      </div>                        
 
 
 
@@ -34,7 +55,8 @@ const CourseDetails = () => {
 
 
     </div>
-  );
+    </>
+  ): <Loading />
 }
 
 export default CourseDetails;
