20c8867740afeb714106c3817fc2f5b3bdf0c003
dpac14 <dipakchute11@gmail.com>
2026-05-07 14:53:35 +0530

    Fix some line

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -9,7 +9,7 @@ const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null)
 
-  const { allCourses,calculateRating } = useContext(AppContext)
+  const { allCourses, calculateRating } = useContext(AppContext)
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id)
@@ -21,42 +21,43 @@ const CourseDetails = () => {
 
   return courseData ? (
     <>
-    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
-      <div className='absolute top-0 left-0 w-full h-spacing-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+      <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+        <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
 
-      {/* let colunm */}
-      <div className='max-w-xl z-10 text-gray-500'>
-        <h1 className='md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800'>{courseData.courseTitle}</h1>
-        <p className='pt-4 md:text-base text-sm '
-         dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0,200)}}></p>
+        {/* let colunm */}
+        <div className='max-w-xl z-10 text-gray-500'>
+          <h1 className='md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800'>{courseData.courseTitle}</h1>
+          <p className='pt-4 md:text-base text-sm '
+            dangerouslySetInnerHTML={{ __html: courseData.courseDescription.slice(0, 200) }}></p>
 
-{/* review and rating  */}
-<div className='flex items-center space-x-2'>
-          <p>{calculateRating(courseData)}</p>
-          <div className='flex'>
-            {[...Array(5)].map((_, i) => (<img key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt='' className='w-3.5 h-33.5' />
-            ))}
-          </div> 
-           <p className='text-blue-600'>({courseData.courseRatings.length} {courseData.courseRatings.length > 1 ?'ratings ': 'rating'})</p>
+          {/* review and rating  */}
+          <div className='flex items-center space-x-2 pt-0 pb-1 text-sm'>
+            <p>{calculateRating(courseData)}</p>
+            <div className='flex'>
+              {[...Array(5)].map((_, i) => (<img key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt='' className='w-3.5 h-33.5' />
+              ))}
+            </div>
+            <p className='text-blue-600'>({courseData.courseRatings.length} {courseData.courseRatings.length > 1 ? 'ratings ' : 'rating'})</p>
 
-           <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' : 'student' }</p>
+            <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' : 'student'}</p>
+          </div>
+          <p className='text-sm'>Course By <span className='text-blue-600 underline'>ChaiOrCode</span></p>
+
+
         </div>
 
 
-      </div>                        
+
+
+        {/* right colunm */}
+        <div></div>
 
 
 
 
-      {/* right colunm */}
-      <div></div>
-
-
-
-
-    </div>
+      </div>
     </>
-  ): <Loading />
+  ) : <Loading />
 }
 
 export default CourseDetails;
