6166a0e41ba1e0e8b8526f820fd9a2967a62ec11
dpac14 <dipakchute11@gmail.com>
2026-05-07 15:29:08 +0530

    added function in AppContexFile

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext()
 
@@ -29,8 +30,30 @@ export const AppContextProvider = (props) => {
         })
         return totalRating / course.courseRatings.length
     }
+    // Function to Calculate Course Chapter Time
+    const calculateChapterTime = (chapter) => {
+        let time = 0
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, { unit: ["h", "m"] })
 
-
+    }
+    // Function to Calculate Course Duration
+    const calculateCourseDuration = (course) => {
+        let time = 0
+        course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] })
+    }
+    // Function to No of Leacture in the Course 
+    const calculateNoOfLectures = (course)=>{
+        let totalLectures =0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent,length;
+            }
+        })
+        return totalLectures;
+    }
 
     useEffect(() => {
         fetchAllCourses()
