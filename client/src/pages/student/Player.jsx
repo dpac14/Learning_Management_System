837a63ecbf04265822d207421f4f525355211191
dpac14 <dipakchute11@gmail.com>
2026-05-19 20:09:32 +0530

    Added Left side

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -5,18 +5,22 @@ import { assets } from '../../assets/assets';
 import humanizeDuration from 'humanize-duration';
 
 const Player = () => {
-  const {enrolledCourses, calculateChapterTime} = useContext(AppContext)
-  const {courseId} = useParams()
-  const [courseData, setCourseData]= useState(null)
-  const [openSection, setOpenSections]= useState({})
-  const [playerData, setPlayerData]= useState(null)
-  
-  const getCourseData =()=>{
-    enrolledCourses.map((course)=>{
-if(course._id === courseId ){
-  setCourseData(course)
-}
-    })
+  const { enrolledCourses, calculateChapterTime } = useContext(AppContext)
+  const { courseId } = useParams()
+  const [courseData, setCourseData] = useState(null)
+  const [openSections, setOpenSections] = useState({})
+  const [playerData, setPlayerData] = useState(null)
+
+  const getCourseData = () => {
+    enrolledCourses.map(
+      (course) => {
+        if (course.__id === courseId) {
+          setCourseData(course)
+        }
+      }
+    )
+
+
   }
 
   const toggleSection = (index) => {
@@ -30,34 +34,35 @@ if(course._id === courseId ){
   }
 
 
-  useEffect(()=>{
+  useEffect(() => {
     getCourseData()
-  },[])
+  }, [enrolledCourses])
   return (
     <>
-      <div className='p-4 sm:p-10 flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36'>
+      <div className='p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36'>
         {/* Left column */}
         <div className='text-gray-800'>
           <h2 className='text-xl font-semibold'>Course Structure</h2>
 
+
           <div className='pt-5'>
-            { courseData && courseData.courseContent.map((chapter, index) => (
+            {courseData && courseData.courseContent.map((chapter, index) => (
               <div key={index} className=' border border-gray-300 bg-white mb-2 rounded'>
                 <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={() => toggleSection(index)}>
                   <div className='flex items-center gap-2'>
-                    <img className={`transform transition-tranform ${openSection[index] ? 'rotate-180' : ''}`}
-                      src={assets.down_arrow_icon} alt="arroe icon" />
+                    <img className={`transform transition-transform ${openSections[index] ? 'rotate-180' : ''}`}
+                      src={assets.down_arrow_icon} alt="arrow icon" />
                     <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                   </div>
-                  <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures -{calculateChapterTime(chapter)}</p>
+                  <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures -  {calculateChapterTime(chapter)}</p>
                 </div>
 
-                <div className={`overflow-hidden transition-all duration-300 ${openSection[index] ? 'max-h-96' : 'max-h-0'}`}>
+                <div className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-screen' : 'max-h-0'}`}>
                   <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                     {chapter.chapterContent.map((lecture, i) => (
                       <li key={i} className='flex items-start gap-2 py-1'>
-                        <img src={false ?  assets.blue_tick_icon : assets.play_icon} alt="paly icon" className='w-4 h-4 mt-1' />
-                        <div className='flex-item-center justify-between w-full text-gray-800 text-xs md:text-default'>
+                        <img src={false ? assets.blue_tick_icon : assets.play_icon} alt="play icon" className='w-4 h-4 mt-1' />
+                        <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
                           <p>{lecture.lectureTitle}</p>
                           <div className='flex gap-2'>
                             {lecture.lectureUrl && <p
@@ -78,9 +83,12 @@ if(course._id === courseId ){
           </div>
 
 
+
         </div>
         {/* Right column */}
-        <div></div>
+        <div>
+          <img src={courseData ? courseData.courseThumbnail : ''} alt="" />
+        </div>
       </div>
     </>
   );
