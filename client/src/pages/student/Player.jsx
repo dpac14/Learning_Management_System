93c091abb0dc39a50f7ba3785f4ed9e94c6dc89d
dpac14 <dipakchute11@gmail.com>
2026-05-20 15:19:19 +0530

    Added right Sec

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -3,6 +3,8 @@ import { AppContext } from '../../context/AppContext';
 import { useParams } from 'react-router-dom';
 import { assets } from '../../assets/assets';
 import humanizeDuration from 'humanize-duration';
+import YouTube from 'react-youtube'
+import Footer from '../../components/students/Footer';
 
 const Player = () => {
   const { enrolledCourses, calculateChapterTime } = useContext(AppContext)
@@ -86,10 +88,27 @@ const Player = () => {
 
         </div>
         {/* Right column */}
-        <div>
-          <img src={courseData ? courseData.courseThumbnail : ''} alt="" />
+        <div className='md:mt-10'>
+          {playerData ? (
+            <div>
+              <YouTube videoId={playerData.lectureUrl.split('/').pop()} iframeClassName='w-full aspect-video' />
+              <div className='flex justify-between items-center mt-1'>
+                <p>
+                  {playerData.chapter}.{playerData.lecture} {playerData.lectureTitle}
+                </p>
+                <button className='text-blue-600'>
+                  {false ? 'Completed' : 'Mark Complete'}
+                </button>
+              </div>
+            </div>
+          )
+            :
+
+            <img src={courseData ? courseData.courseThumbnail : ''} alt="" />
+          }
         </div>
       </div>
+      <Footer />
     </>
   );
 
