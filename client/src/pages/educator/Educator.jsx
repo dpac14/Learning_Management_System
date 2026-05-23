9445c37fa1caa0081e42c5a309e45f7a6597aa5f
dpac14 <dipakchute11@gmail.com>
2026-05-23 19:21:45 +0530

    Addeed Sidebar

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/educator/Navbar';
+import SideBar from '../../components/educator/SideBar';
+import Footer from '../../components/educator/Footer';
 
 const Educator = () => {
   return (
     <div className='text-default min-h-screen bg-white'>
       <Navbar />
-      <div>
-        {<Outlet />}
+      <div className='flex'>
+        <SideBar />
+        <div className='flex-1'>
+           {<Outlet />}
+        </div>
+       
       </div>
+      <Footer />
     </div>
   );
 }
