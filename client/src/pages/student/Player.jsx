012d5589f7350b49e13b913e08bd8d79742ec7c4
dpac14 <dipakchute11@gmail.com>
2026-05-29 23:13:19 +0530

    Added New line

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -61,13 +61,13 @@ const Player = () => {
                   <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                     {chapter.chapterContent.map((lecture, i) => (
                       <li key={i} className='flex items-start gap-2 py-1'>
-                        <img src={false ? assets.blue_tick_icon : assets.play_icon}alt="paly icon" className='w-4 h-4 mt-1' />
+                        <img src={false ? assets.blue_tick_icon : assets.play_icon} alt="paly icon" className='w-4 h-4 mt-1' />
                         <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
                           <p>{lecture.lectureTitle}</p>
                           <div className='flex gap-2'>
                             {lecture.lectureUrl && <p
                               onClick={() => setPlayerData({
-                                ...lecture, chapter: index + 1, lecture:i + 1
+                                ...lecture, chapter: index + 1, lecture: i + 1
                               })}
                               className='text-blue-500 cursor-pointer'>Watch </p>}
                             <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
