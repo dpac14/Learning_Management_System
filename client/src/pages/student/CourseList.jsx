8cebda7834367dbf634bb66d48adc37a81a774ab
dpac14 <dipakchute11@gmail.com>
2026-06-01 00:01:05 +0530

    Change the some line

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -10,21 +10,21 @@ const CourseList = () => {
 
   const { navigate, allCourses } = useContext(AppContext)
   const { input } = useParams()
-  const [filteredCourse, setFilteredCourse] =useState([])
-  useEffect(()=>{
-    if(allCourses && allCourses.length > 0){
+  const [filteredCourse, setFilteredCourse] = useState([])
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0) {
       const tempCourses = allCourses.slice()
 
       input ?
-      setFilteredCourse(
-        tempCourses.filter(
-          item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+        setFilteredCourse(
+          tempCourses.filter(
+            item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+          )
         )
-      )
-      : setFilteredCourse(tempCourses)
+        : setFilteredCourse(tempCourses)
     }
 
-  },[allCourses, input])
+  }, [allCourses, input])
 
   return (
     <>
@@ -41,14 +41,14 @@ const CourseList = () => {
         {
           input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600'>
             <p>{input}</p>
-            <img src={assets.cross_icon} alt=""className='cursor-pointer' onClick={()=>navigate('/course-list')} />
+            <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={() => navigate('/course-list')} />
           </div>
         }
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0' >
           {filteredCourse.map((course, index) => <CourseCard key={index} course={course} />)}
         </div>
       </div>
-     <Footer />
+      <Footer />
     </>
   );
 }
